2c425d022552942dedd98d78af58a66389a7db38
Andrea Walsh <andreawalsh@Carters1MacBook.attlocal.net>
2022-03-24 17:29:59 -0500

    Second commit

--- addToZero.js
+++ addToZero.js
@@ -1,34 +1,38 @@
 // Starting array
-let array = [28, 43, -12, 30, 4, 0, -4];
+let array = [28, 43, -12, 30, 4, 0, 13];
 let reverseArray = [];
 // Write your solution below:
 
-// Separate For loops
-for (let i = array.length; i > 0; i--) {
-    let num1 = array[i - 1];
-    reverseArray.push(num1);
-}  
+// Separate For loops attempt
+// for (let i = array.length; i > 0; i--) {
+//     let num1 = array[i - 1];
+//     reverseArray.push(num1);
+// }  
 
-for (let i = 0; i < array.length; i++) {
-      let index = 0;
-      if (array[i] + reverseArray[index] === 0) {
-        console.log("True");
-      } else {
-        null
-      }
-    index++;
-  }
+// for (let i = 0; i < array.length; i++) {
+//       let index = 0;
+//       if (array[i] + reverseArray[index] === 0) {
+//         console.log("True");
+//       } 
+//     index++;
+//   }
+
+
+
+
+
 
   // Nested For loop
-let result = "";
+let result = "False";
 for(let i = 0; i < array.length; i++) {
   for(let j = 0; j < array.length; j++) {
     if (i !== j) {
       if (array[i] + array[j] === 0) {
       result = "True";
-    }
+    } 
   }
 }
 }
 console.log(result);
+
 
